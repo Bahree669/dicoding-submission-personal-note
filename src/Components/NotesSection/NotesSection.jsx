62f8e8f3fd5e7c772d49c6963b62209b1d59be2f
Bahree <bahree36@outlook.com>
2022-07-23 14:34:26 +0700

    changed the typography in the title

--- src/Components/NotesSection/NotesSection.jsx
+++ src/Components/NotesSection/NotesSection.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import NotesGridLayout from "../NotesGridLayout/NotesGridLayout";
 import Note from "../Note/Note";
 
+import "./NotesSection.css";
+
 function NotesSection({ notes }) {
     return (
-        <NotesGridLayout>
-            {notes.map((note) => (
-                <Note key={note.id} title={note.title} body={note.body} createdAt={note.createdAt} />
-            ))}
-        </NotesGridLayout>
+        <section className='notes-section'>
+            <h2>Your notes</h2>
+
+            <NotesGridLayout>
+                {notes.map((note) => (
+                    <Note key={note.id} title={note.title} body={note.body} createdAt={note.createdAt} />
+                ))}
+            </NotesGridLayout>
+        </section>
     );
 }
 
