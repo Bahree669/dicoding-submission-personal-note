b7fc4b8775c7860b81be9eba61e3596ac24a4f45
Bahree <bahree36@outlook.com>
2022-09-24 10:43:53 +0700

    restart the project from start

--- src/Components/index.js
+++
@@ -1,8 +0,0 @@
-import Header from "./Header/Header";
-import NoNotesNotice from "./NoNotesNotice/NoNotesNotice";
-import InputForm from "./InputForm/InputForm";
-import NotesGridLayout from "./NotesGridLayout/NotesGridLayout";
-import NotesSection from "./NotesSection/NotesSection";
-import ArchivesSection from "./ArchivesSection/ArchivesSection";
-
-export { Header, ArchivesSection, NotesSection, NotesGridLayout, NoNotesNotice, InputForm };
